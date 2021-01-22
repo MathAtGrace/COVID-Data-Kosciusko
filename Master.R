092389f03509d8a5effc4b8fcd96955888b10400
Ryan Johnson <johnsor@grace.edu>
2021-01-22 13:51:38 -0500

    fixed the "t"

--- Master.R
+++ Master.R
@@ -6,7 +6,7 @@ source("Indiana_Data.R")
 source("Test_Data.R")
 
 #The COVID Dashboard
-fname = paste("G:/Shared drives/Dashboard - COVID-19/Grace inernal ",
+fname = paste("G:/Shared drives/Dashboard - COVID-19/Grace internal ",
               format(Sys.Date()), ".pdf", sep = "")
 render("Grace_internal4.Rmd", output_file = fname)
 
