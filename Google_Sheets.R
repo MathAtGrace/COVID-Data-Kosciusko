ac71408c0628f3e4810eb6acf409b9549a0dba96
Ryan Johnson <johnsor@grace.edu>
2021-01-25 15:31:14 -0500

    jan 25 update, including all reworking of student data with the new spring spreadsheet

--- Google_Sheets.R
+++ Google_Sheets.R
@@ -2,30 +2,63 @@
 library(googlesheets4)
 
 #Student Data
-S <- read_sheet("https://docs.google.com/spreadsheets/d/1Jwp6_vxG2l3JQU-hiZ6XBmI_74H6DcDVnF4g5DBn9ic/edit#gid=0",
-                skip = 2,
-                col_types = paste0("ncc",
-                                   "cc",
-                                   "ccc",
-                                   "ccDD",
-                                   "DDD",
-                                   "DDc",
-                                   "DDc",
-                                   "c",
-                                   "ccccc",
-                                   "ccc"),
-                col_names = c("ID", "Last_Name", "First_Name",
-                              "Last_Care_Call_Date", "Email_Sent",
-                              "Close_Contact", "Gender", "Phone",
-                              "Grace_Space", "Location", "TQS", "Exposure_Date",
-                              "Quarantine", "Isolated", "Symptons_Started",
-                              "Test_Date", "Result_Date", "Test_Result",
-                              "End_Date", "Reslease_Email_Sent", "Results_Recd",
-                              "Connie_Email",
-                              "RD_RA", "Athlete", "Meals", "Complete", "Notes",
-                              "Extra", "Care_Call_Notes", "Ryan"))
+S_fall <- read_sheet("https://docs.google.com/spreadsheets/d/1Jwp6_vxG2l3JQU-hiZ6XBmI_74H6DcDVnF4g5DBn9ic/edit#gid=0",
+                skip = 1)
+#,
+#                col_types = paste0("ncc",
+#                                   "cc",
+#                                   "ccc",
+#                                   "ccDD",
+#                                   "DDD",
+#                                   "DDc",
+#                                   "DDc",
+#                                   "c",
+#                                   "ccccc",
+#                                   "ccc"),
+#                col_names = c("ID", "Last_Name", "First_Name",
+#                              "Last_Care_Call_Date", "Email_Sent",
+#                              "Close_Contact", "Gender", "Phone",
+#                              "Grace_Space", "Location", "TQS", "Exposure_Date",
+#                              "Quarantine", "Isolated", "Symptons_Started",
+#                              "Test_Date", "Result_Date", "Test_Result",
+#                              "End_Date", "Reslease_Email_Sent", "Results_Recd",
+#                              "Connie_Email",
+#                              "RD_RA", "Athlete", "Meals", "Complete", "Notes",
+#                              "Extra", "Care_Call_Notes", "Ryan"))
 #Save the Student Data
-save(S, file = "S.RData")
+
+#Backup Plan - The Copycat spreadsheet
+#S_fall <- read_sheet("https://docs.google.com/spreadsheets/d/1hYSSaiyIbV_pjBs3ljTwF9kf5YI0Ewj_bNEt7tNYMtA/edit#gid=0",
+#                     skip = 1)
+
+#Save the fall data
+save(S_fall, file = "SF.RData")
+
+S_spring <- read_sheet("https://docs.google.com/spreadsheets/d/107-RZMp8JifihWCQymwHVtJ7teUtVQL1cSC4O1H0fLo/edit?ts=600b0c97#gid=0",
+                     skip = 1)
+
+save(S_spring, file = "SS.Rdata")
+#                     col_types = paste0("ncc", #ABC
+#                                        "DD", #DE
+#                                        "ccc", #FGH
+#                                        "cccc", #IJKL
+#                                        "DDD", #MNO
+#                                        "cDD", #PQR
+#                                        "cDD", #STU
+#                                        "ccc", #VWX
+#                                        "cccc", #Y Z AA AB
+#                                        "c"), #AC
+#                     col_names = c("ID", "Last_Name", "First_Name", #ABC
+#                                   "Last_QI_Form_Sent", "Care_Call", #DE
+#                                   "Email_Sent", "Q_Exp_Name", "Gender", #FGH
+#                                   "Phone", "Grace_Space", "Needs_Meal", "Location", #IJKL
+#                                   "TQS", "Exposure", "QI_Started", #MNO
+#                                   "Asymptomatic", "Symptom_Start", "Test_Date", #PQR
+#                                   "Test_Result", "Possible_End", "Actual_End", #STU
+#                                   "Results_Received", "Release_Email_Sent", "Connie_Emailed", #VWX
+#                                   "RD", "Athlete", "Complete", "Notes", #Y Z AA AB
+#                                   "Health_Center")) #AC
+
 
 #Employee Data
 E <- read_sheet("https://docs.google.com/spreadsheets/d/1M1rKYVYg8bGqYlHjmh51XRNK4JLY5TjMPbrw9_OUaFE/edit#gid=0",
--- Testing_Data.R
+++ Testing_Data.R
@@ -1,8 +1,9 @@
-require(tidyverse)
+require(tidyverse, janitor)
 
-T <- read.csv("M:/COVID_dashboard/COVID_dashboard_test_results.csv",
+load("T.RData")
+T3 <- read.csv("M:/COVID_dashboard/COVID_dashboard_test_results.csv",
               col.names = c("ID", "Test_Result", "Test_Date", "State", "At_Health_Center")) %>%
-  mutate(Test_Result = factor(tolower(Test_Result), levels = c("p", "n")),
+  mutate(Test_Result = factor(tolower(Test_Result), levels = c("p", "n", "i")),
          Test_Date = as.Date(Test_Date),
          State = case_when(
            is.na(State) ~ FALSE,
@@ -12,6 +13,12 @@ T <- read.csv("M:/COVID_dashboard/COVID_dashboard_test_results.csv",
            At_Health_Center == "N" ~ FALSE,
            TRUE ~ TRUE
          )) %>%
-  select(-State)
+  clean_names() %>%
+  select(-state)
+
+T <- bind_rows(T, T3) %>%
+  distinct(ID, .keep_all = TRUE)
 
 save(T, file = "T.Rdata")
+
+
