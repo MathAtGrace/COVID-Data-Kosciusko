ea6ef4f8009cd284b0047db67a59ab9c87237156
Ryan Johnson <johnsor@grace.edu>
2021-01-26 09:16:41 -0500

    jan 26 update

--- Testing_Data.R
+++ Testing_Data.R
@@ -1,23 +1,16 @@
-require(tidyverse, janitor)
+library(tidyverse)
+library(janitor)
 
-load("T.RData")
-T3 <- read.csv("M:/COVID_dashboard/COVID_dashboard_test_results.csv",
+T <- read.csv("M:/COVID_dashboard/COVID_dashboard_test_results.csv",
               col.names = c("ID", "Test_Result", "Test_Date", "State", "At_Health_Center")) %>%
   mutate(Test_Result = factor(tolower(Test_Result), levels = c("p", "n", "i")),
          Test_Date = as.Date(Test_Date),
-         State = case_when(
-           is.na(State) ~ FALSE,
-           TRUE ~ State
-         ),
          At_Health_Center = case_when(
            At_Health_Center == "N" ~ FALSE,
            TRUE ~ TRUE
          )) %>%
   clean_names() %>%
   select(-state)
-
-T <- bind_rows(T, T3) %>%
-  distinct(ID, .keep_all = TRUE)
 
 save(T, file = "T.Rdata")
 
