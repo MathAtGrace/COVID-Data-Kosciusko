359da581e4ff3a050dc126f3ff285ee7c40e2c10
Ryan Johnson <johnsor@grace.edu>
2021-02-05 07:59:15 -0500

    block false positives in the testing source

--- Testing_Data.R
+++ Testing_Data.R
@@ -6,9 +6,13 @@ T <- read.csv("M:/COVID_dashboard/COVID_dashboard_test_results.csv") %>%
   clean_names()%>%
   rename(id = i_id_num,
          at_hc = tested_at_health_center) %>%
-  mutate(test_result = (test_result == "P"),
-         test_time = as_datetime(test_dt),
-         employee = case_when(employee == "Y" ~ TRUE, TRUE ~ FALSE))%>%
+  mutate(test_time = as_datetime(test_dt),
+         employee = case_when(employee == "Y" ~ TRUE, TRUE ~ FALSE),
+         test_result = case_when(
+           id == 1618586 & test_time == as_datetime("2021-02-03 16:04:25") ~ FALSE,
+           id == 1610566 & test_time == as_datetime("2021-01-25 12:59:04") ~ FALSE,
+           TRUE ~ (test_result == "P")
+         ))%>%
   select(-sent_to_state_dt)
 
 save(T, file = "T.Rdata")
