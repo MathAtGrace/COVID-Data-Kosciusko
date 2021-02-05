9ca0655ad35e848be9e20e3e33646daedd4607bf
Ryan Johnson <johnsor@grace.edu>
2021-02-05 07:42:19 -0500

    date to datetime

--- Testing_Data.R
+++ Testing_Data.R
@@ -1,13 +1,13 @@
 library(tidyverse)
 library(janitor)
+library(lubridate)
 
 T <- read.csv("M:/COVID_dashboard/COVID_dashboard_test_results.csv") %>%
   clean_names()%>%
   rename(id = i_id_num,
-         test_date = test_dt,
          at_hc = tested_at_health_center) %>%
   mutate(test_result = (test_result == "P"),
-         test_date = as.Date(test_date),
+         test_time = as_datetime(test_dt),
          employee = case_when(employee == "Y" ~ TRUE, TRUE ~ FALSE))%>%
   select(-sent_to_state_dt)
 
