71b8444bfe11eb9ebc0786b6dddafbd4c0d9793a
Ryan Johnson <johnsor@grace.edu>
2021-03-12 12:36:33 -0500

    march 12 update

--- Testing_Data.R
+++ Testing_Data.R
@@ -12,6 +12,9 @@ T <- read.csv("M:/COVID_dashboard/COVID_dashboard_test_results.csv") %>%
            id == 1618586 & test_time == as_datetime("2021-02-03 16:04:25") ~ FALSE,
            id == 1610566 & test_time == as_datetime("2021-01-25 12:59:04") ~ FALSE,
            id == 1595908 & test_time == as_datetime("2021-02-16 12:37:16") ~ FALSE,
+           id == 1649510 & test_time == as_date("2021-03-11") ~ FALSE,
+           id == 1595183 & test_time == as_date("2021-03-11") ~ FALSE,
+           id == 1646082 & test_time == as_date("2021-03-11") ~ FALSE,
            TRUE ~ (test_result == "P")
          ))%>%
   select(-sent_to_state_dt)
