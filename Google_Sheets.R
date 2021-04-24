e2db01e38165fad235649ef4c9c9c377bf67b3f0
Ryan Johnson <johnsor@grace.edu>
2021-04-23 23:07:56 -0400

    google

--- Google_Sheets.R
+++ Google_Sheets.R
@@ -1,14 +1,6 @@
 #Try to only do this once or twice a day
 library(googlesheets4)
 
-#Student Data
-#S_fall <- read_sheet("https://docs.google.com/spreadsheets/d/1Jwp6_vxG2l3JQU-hiZ6XBmI_74H6DcDVnF4g5DBn9ic/edit#gid=0",
- #               skip = 1)
-
-
-
-#Save the fall data
-#save(S_fall, file = "SF.RData")
 
 S_spring <- read_sheet("https://docs.google.com/spreadsheets/d/107-RZMp8JifihWCQymwHVtJ7teUtVQL1cSC4O1H0fLo/edit?ts=600b0c97#gid=0",
                      sheet = "Quarantine/Isolation", skip = 1)
