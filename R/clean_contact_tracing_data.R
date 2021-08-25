de9399a0c8774ced674da88187e1ca4e0a147dd6
Johnson <johnsor@grace.edu>
2021-08-25 13:44:36 -0400

    move files to data folder

--- R/clean_contact_tracing_data.R
+++ R/clean_contact_tracing_data.R
@@ -9,8 +9,6 @@ load("data/SS.Rdata")
 load("data-raw/E.Rdata")
 
 #Important dates
-f20 <- as.Date("2020-07-20")
-s21 <- as.Date("2021-01-01")
 f21 <- as.Date("2021-08-14")
 load("data/this_day.Rdata")
 yest <- this_day-1
